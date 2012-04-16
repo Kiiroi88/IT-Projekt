b3e82c443dfc8b37630270faf539dc0611c8eaee
Magnus Koch <magnus.koch88@gmail.com>
2012-04-16 14:03:09 +0200

    Test

---
+++ test.java
@@ -0,0 +1,13 @@
+package testpkg;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
