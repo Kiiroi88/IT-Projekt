b2ddf389f7bf4ec11b99410b9ebda0c6867e93f9
Leonid Sibirzeff <Leonids@kth.se>
2012-04-23 16:09:21 +0200

    Första version av gui

---
+++ GUI/GUI_LOG_IN/usrTxt.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI_LOG_IN;
+
+/**
+ *
+ * @author DELL
+ */
+class usrTxt {
+    
+}
