db06e661ca8adc091910a7cf2c5d3ee76cf5e36d
Andreas Roth <lullelo@hotmail.com>
2012-04-24 08:44:45 +0200

    ändrade package

--- GUI/GUI_COUNTER/Counter.java
+++ GUI/GUI_COUNTER/Counter.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI.GUI_COUNTER;
+package GUI_COUNTER;
 
 /**
  *
--- GUI/GUI_LOG_IN/usrTxt.java
+++ GUI/GUI_LOG_IN/usrTxt.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI.GUI_LOG_IN;
+package GUI_LOG_IN;
 
 /**
  *
