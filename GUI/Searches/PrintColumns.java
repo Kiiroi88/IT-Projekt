0e8641fa1889e3685ff30cf96061578b34e0fd56
Andreas Roth <lullelo@hotmail.com>
2012-04-27 08:54:47 +0200

    PrintColums för att stock ska fungera

---
+++ GUI/Searches/PrintColumns.java
@@ -0,0 +1,86 @@
+package Searches;
+
+import java.sql.*;
+
+public class PrintColumns {
+
+	public String[][] Print() {
+
+		String[][] table = null;
+		String url = "jdbc:mysql://184.172.173.42:3306/chichiu_ITProjekt";
+		Connection con;
+		String query = "select * from Product";
+		String query2 = "select * from Product";
+		Statement stmt;
+		Statement stmt2;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+		} catch (java.lang.ClassNotFoundException e) {
+			System.err.print("ClassNotFoundException: ");
+			System.err.println(e.getMessage());
+		}
+
+		try {
+			con = DriverManager.getConnection(url, "chichiu_anroth", "it12345");
+
+			stmt = con.createStatement();
+			stmt2 = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery(query);
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			// PrintColumnTypes.printColTypes(rsmd);
+			System.out.println("");
+
+			ResultSet rowcount = stmt2.executeQuery(query2);
+			int rc = 0;
+
+			while (rowcount.next()) {
+				rc++;
+
+			}
+
+			int y = 0;
+			int numberOfColumns = rsmd.getColumnCount();
+			table = new String[rc][numberOfColumns];
+
+			while (rs.next()) {
+				
+				for (int j = 1; j < numberOfColumns; j++) {
+					String columnValue = rs.getString(j);
+					table[y][j - 1] = columnValue;
+				}
+				y++;
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException ex) {
+			System.err.print("SQLException: ");
+			System.err.println(ex.getMessage());
+		}
+		return table;
+
+	}
+
+}
+
+// class PrintColumnTypes {
+//
+// public static void printColTypes(ResultSetMetaData rsmd)
+// throws SQLException {
+// int columns = rsmd.getColumnCount();
+// for (int i = 1; i <= columns; i++) {
+// int jdbcType = rsmd.getColumnType(i);
+// String name = rsmd.getColumnTypeName(i);
+// System.out.print("Column " + i + " is JDBC type " + jdbcType);
+// System.out.println(", which the DBMS calls " + name);
+//
+// }
+// }
+//
+// }
+
---
+++ GUI/Searches/Search_Fun.java
@@ -0,0 +1,406 @@
+package Searches;
+
+import java.sql.*;
+import java.util.*;
+
+public class Search_Fun 
+{
+	ArrayList idList = new ArrayList();
+	public ArrayList <String> groceryList = new ArrayList<String>();
+	ArrayList articleList = new ArrayList();
+	ArrayList unitList = new ArrayList();
+	ArrayList quantityList = new ArrayList();
+	ArrayList amountFRPList = new ArrayList();
+	ArrayList priceList = new ArrayList();
+	ArrayList typeList = new ArrayList();
+	ArrayList storedAtList = new ArrayList ();
+	ArrayList testList = new ArrayList ();
+	int size = 0;
+
+	// DB connection variable
+	protected static Connection con;
+
+	// DB access variables
+	private String URL = "jdbc:mysql://184.172.173.42:3306/chichiu_ITProjekt";
+	private String driver = "com.mysql.jdbc.Driver";
+	private String userID = "chichiu_miob";
+	private String password = "it12345";
+
+	// method for establishing a DB connection
+	public void connect()
+	{
+		try
+		{
+			// register the driver with DriverManager
+			Class.forName(driver);
+			//create a connection to the database
+			con = DriverManager.getConnection(URL, userID, password);
+			System.out.println("Connection established! ");
+			// Set the auto commit of the connection to false.
+			// An explicit commit will be required in order to accept
+			// any changes done to the DB through this connection.
+			con.setAutoCommit(false);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	// Method for retreival of information in table stored 
+	// in the database (MySQL)
+	public ArrayList select () throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		searchQuery = "SELECT Name FROM Product"; // SQL command to retrieve all the groceries, set into query variable 
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+		System.out.println("All groceries from database: ");
+
+		while(resultSet.next())
+		{
+			groceryList.add(resultSet.getString("Name"));
+
+		}
+
+		System.out.println(groceryList);
+		return groceryList;
+	}
+	public ArrayList selectArticle (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT RefNr FROM Product WHERE Name='" + keyword +"'";
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				articleList.add(resultSet.getInt("RefNr"));
+			}
+		}
+		System.out.println("Article information " + articleList);
+		return articleList;
+	}
+
+	//Several methods for get content from column
+
+	public ArrayList selectID (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT ID FROM Product WHERE Name='" + keyword +"'";
+
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				idList.add(resultSet.getInt("ID"));
+			}
+		}
+		System.out.println("ID information " + idList);
+		return idList;
+	}
+
+	public ArrayList selectUnit (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Unit FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				unitList.add(resultSet.getString("Unit"));
+			}
+		}
+		System.out.println("Unit information " + unitList);
+		return unitList;
+	}
+
+	public ArrayList selectQuantity (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Quantity FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				quantityList.add(resultSet.getInt("Quantity"));
+
+			}
+		}
+		System.out.println("Quantity information " + quantityList);
+		return quantityList;
+	}
+
+	public ArrayList selectFRP (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Amount FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				amountFRPList.add(resultSet.getInt("Amount"));
+			}
+		}
+		System.out.println("Amount FRP information " + amountFRPList);
+		return amountFRPList;
+	}
+
+	public ArrayList selectPrice (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Price FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				priceList.add(resultSet.getInt("Price"));
+			}
+		}
+		System.out.println("Price information " + priceList);
+		return priceList;
+	}
+
+	public ArrayList selectType (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Type FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				typeList.add(resultSet.getString("Type"));
+			}
+		}
+		System.out.println("Type information " + typeList);
+		return typeList;
+	}
+
+	public ArrayList selectStoredAt (ArrayList <String> input) throws Exception
+	{
+		String searchQuery;
+		Statement statement;
+		ResultSet resultSet;
+
+		statement = con.createStatement(); // Creation of statement associated with the connection object
+
+		// Loop for article based on matched groceries
+		for(int i=0; i < input.size(); i++)
+		{
+			String keyword = input.get(i);
+			searchQuery = "SELECT Space FROM Product WHERE Name='" + keyword +"'";
+
+			resultSet = statement.executeQuery(searchQuery); // Execute the SQL statement that is saved in the query variable
+
+			while(resultSet.next())
+			{
+				storedAtList.add(resultSet.getInt("Space"));
+			}
+		}
+		System.out.println("Stored at information " + storedAtList);
+		return storedAtList;
+	}
+
+	//Creates a table from retrieved columns from database  
+	public Object[][] combine (ArrayList input) throws Exception
+	{
+		selectArticle(input);
+		selectID(input);
+		selectUnit(input);
+		selectQuantity(input);
+		selectFRP(input);
+		selectPrice(input);
+		selectType(input);
+		selectStoredAt(input);
+
+		size = input.size();
+		int row = size;
+		int column = 9;
+		Object [][] table = new Object [row][column];
+
+		ArrayList change = null;
+
+		for(int i = 0; i < column; i++){
+
+			if(i == 0)
+			{
+
+				change = idList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				}
+
+			}
+
+			if(i == 1)
+			{
+
+				change = input;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				} 
+			}
+
+			if(i == 2)
+			{
+
+				change = articleList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				}
+			}
+
+			if(i == 3)
+			{
+
+				change = unitList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				}
+			}
+
+			if(i == 4)
+			{
+
+				change = quantityList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				} 
+			}
+
+			if(i == 5)
+			{
+
+				change = amountFRPList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				}
+			}
+
+			if(i == 6)
+			{
+
+				change = priceList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				} 
+			}
+
+			if(i == 7)
+			{
+
+				change = typeList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				}
+			}
+
+			if(i == 8)
+			{
+
+				change = storedAtList;
+				for(int j = 0; j < row; j++)
+				{
+					table[j][i] = change.get(j);
+				} 
+			}
+
+		}
+
+		//Temporary for loop to print the table in the console	
+		for (int x = 0; x < size; x++){
+			System.out.println();
+			for (int y = 0; y < 9; y++) {
+				System.out.print(" " + table[x][y]);
+				System.out.print(" ");
+
+			}
+		}	
+
+
+		return table;
+	}
+}
+
